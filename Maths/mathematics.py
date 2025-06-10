9b6e3c13f1886357f49e07e658b35d0390428158
filipapedra <filipagpedra@gmail.com>
2025-06-10 12:07:19 -0400

    added summation and subtraction to serverpy

---
+++ Maths/mathematics.py
@@ -0,0 +1,15 @@
+```python
+def summation(a, b):
+    result = a + b
+    return result
+
+```python
+def subtraction(a, b):
+    result = a - b
+    return result
+```
+```python
+def multiplication(a, b):
+    result = a * b
+    return result
+```
