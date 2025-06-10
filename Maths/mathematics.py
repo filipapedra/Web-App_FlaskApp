95f7f4cee0a5b94b2d895b01c8d2dbbb90ab2c36
filipapedra <filipagpedra@gmail.com>
2025-06-10 12:46:45 -0400

    corrected synthax in mathematics

--- Maths/mathematics.py
+++ Maths/mathematics.py
@@ -1,15 +1,11 @@
-```python
 def summation(a, b):
     result = a + b
     return result
 
-```python
 def subtraction(a, b):
     result = a - b
     return result
-```
-```python
+
 def multiplication(a, b):
     result = a * b
     return result
-```
